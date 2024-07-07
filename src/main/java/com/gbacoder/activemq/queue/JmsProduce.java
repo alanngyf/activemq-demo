59f252a510c7e007343a2a3b19b15c0e4d2fadc6
Alan Ng <alanng@berkeley.edu>
2024-07-06 20:26:07 -0500

    Initial commit

---
+++ src/main/java/com/gbacoder/activemq/queue/JmsProduce.java
@@ -0,0 +1,11 @@
+package com.gbacoder.activemq.queue;
+
+/**
+ * @author alanulog
+ * @create 7/6/2024 8:24 PM
+ */
+public class JmsProduce {
+    public static void main(String[] args) {
+        
+    }
+}
